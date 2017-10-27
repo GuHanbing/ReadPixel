bbda0ad6a28c89a4f03c7e8166f9109231d2693a
GuHanbing <17706179990@163.com>
2017-10-27 18:01:13 +0800

    Add a choice for image input
    Signed-off-by: GuHanbing <17706179990@163.com>

--- main.cpp
+++ main.cpp
@@ -3,42 +3,68 @@
 #include <opencv2/core/core.hpp>
 #include <vector>
 #include<iostream>
+#include<string>
 using namespace cv;
 using namespace std;
-bool selectFlag = false;  //
+
+#define WINDOW_NAME "目标图像"
 /*------------------------------------------------------/
  /功能：鼠标处理事件函数
  /event:鼠标事件  x,y: 鼠标当前坐标 flag:
  /param：函数指针
  /------------------------------------------------------*/
 void OnMouseMove(int event, int x, int y, int flag, void* param = NULL);
-
+bool selectFlag = false;
 
 Mat frame,HSVframe;
 bool stop=false;
 void on_Matching(Mat g_srcImage, Mat g_templateImage);
 int main()
 {
-    int video;
-    cout<<"input No of Video:"<<endl;
-    cin>>video;
-    VideoCapture cap(video);
-    if (!cap.isOpened())
+    int choice=0;
+    int video=0;
+    VideoCapture cap;
+    cout<<"video(input:0) or image(input:1)?"<<endl;
+    cin>>choice;
+    
+    if(choice==1)          //显示图像
     {
-        return -1;
+        cout<<"input filename:"<<endl;
+        string file;
+        cin>>file;
+        frame=imread(file);
+        if(frame.rows==0)
+        {
+            cout<<"can't find"<<endl;
+            return -1;
+        }
+    }
+    else                 //显示视频
+    {
+        cout<<"input No of Video:"<<endl;
+        cin>>video;
+        cap.open(video);
+        if (!cap.isOpened())
+        {
+            cout<<"Open camera failed."<<endl;
+            return -1;
+        }
+        cap>>frame;
     }
     
-    cap>>frame;
     cvtColor(frame, HSVframe, COLOR_BGR2HSV);
-    namedWindow("选择目标", 1);
-    setMouseCallback("选择目标", OnMouseMove, NULL);
-    imshow("选择目标", HSVframe);
+    namedWindow(WINDOW_NAME, 1);
+    setMouseCallback(WINDOW_NAME, OnMouseMove, NULL);
+    imshow(WINDOW_NAME, HSVframe);
     
     while (!stop)
     {
-        cap>>frame;
-        cvtColor(frame, HSVframe, COLOR_BGR2HSV);
-        imshow("选择目标", frame);
+        if(choice==0)
+        {
+            cap>>frame;
+            cvtColor(frame, HSVframe, COLOR_BGR2HSV);
+        }
+        imshow(WINDOW_NAME, frame);
         waitKey(30);
     }
     return 0;
